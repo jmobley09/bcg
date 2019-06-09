bd41a7f36d8aa6fcb4273c76d4c96de426861e48
jmobley <joshua.mobley.jm@gmail.com>
2019-06-09 19:14:25 -0400

    linting correction

--- bcg-client/src/components/NavBar.vue
+++ bcg-client/src/components/NavBar.vue
@@ -1,22 +1,38 @@
 <template>
   <span>
-    <v-navigation-drawer app v-model="drawer" class="orange lighten-1" dark disable-resize-watcher>
+    <v-navigation-drawer
+      app
+      v-model="drawer"
+      class="orange lighten-1"
+      dark
+      disable-resize-watcher
+    >
       <v-list>
         <template v-for="(item, index) in items">
           <v-list-tile :key="index">
-            <v-list-tile-content @click="router.push(item.destination)">{{ item.title }}</v-list-tile-content>
+            <v-list-tile-content @click="router.push(item.destination)">{{
+              item.title
+            }}</v-list-tile-content>
           </v-list-tile>
           <v-divider :key="`divider-${index}`"></v-divider>
         </template>
       </v-list>
     </v-navigation-drawer>
     <v-toolbar app color="orange" dark>
-      <v-toolbar-side-icon class="hidden-md-and-up" @click="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-side-icon
+        class="hidden-md-and-up"
+        @click="drawer = !drawer"
+      ></v-toolbar-side-icon>
       <v-spacer class="hidden-md-and-up"></v-spacer>
       <v-toolbar-title>{{ appTitle }}</v-toolbar-title>
       <v-spacer class="hidden-sm-and-down"></v-spacer>
       <!-- Home Button -->
-      <v-btn flat class="hidden-sm-and-down singlenav" @click="$router.push('/')">Home</v-btn>
+      <v-btn
+        flat
+        class="hidden-sm-and-down singlenav"
+        @click="$router.push('/')"
+        >Home</v-btn
+      >
       <!-- About Dropdown -->
       <v-menu :close-on-content-click="true" :nudge-width="200" offset-x>
         <template v-slot:activator="{ on }">
@@ -26,17 +42,23 @@
           <v-list>
             <v-list-tile>
               <v-list-tile-content>
-                <v-btn flat @click="$router.push({ name: 'drlamm' })">Doctor Lamm</v-btn>
+                <v-btn flat @click="$router.push({ name: 'drlamm' })"
+                  >Doctor Lamm</v-btn
+                >
               </v-list-tile-content>
             </v-list-tile>
             <v-list-tile>
               <v-list-tile-content>
-                <v-btn flat @click="$router.push({ name: 'staff' })">Staff</v-btn>
+                <v-btn flat @click="$router.push({ name: 'staff' })"
+                  >Staff</v-btn
+                >
               </v-list-tile-content>
             </v-list-tile>
             <v-list-tile>
               <v-list-tile-content>
-                <v-btn flat @click="$router.push({ name: 'clinical' })">Clinical Trials</v-btn>
+                <v-btn flat @click="$router.push({ name: 'clinical' })"
+                  >Clinical Trials</v-btn
+                >
               </v-list-tile-content>
             </v-list-tile>
           </v-list>
@@ -51,17 +73,23 @@
           <v-list>
             <v-list-tile>
               <v-list-tile-content>
-                <v-btn flat @click="$router.push({ name: 'biopsy' })">Biopsy</v-btn>
+                <v-btn flat @click="$router.push({ name: 'biopsy' })"
+                  >Biopsy</v-btn
+                >
               </v-list-tile-content>
             </v-list-tile>
             <v-list-tile>
               <v-list-tile-content>
-                <v-btn flat @click="$router.push({ name: 'cystectomy' })">Cystectomy</v-btn>
+                <v-btn flat @click="$router.push({ name: 'cystectomy' })"
+                  >Cystectomy</v-btn
+                >
               </v-list-tile-content>
             </v-list-tile>
             <v-list-tile>
               <v-list-tile-content>
-                <v-btn flat @click="$router.push({ name: 'turbt' })">TURBT</v-btn>
+                <v-btn flat @click="$router.push({ name: 'turbt' })"
+                  >TURBT</v-btn
+                >
               </v-list-tile-content>
             </v-list-tile>
             <v-list-tile>
@@ -73,9 +101,16 @@
         </v-card>
       </v-menu>
       <!-- Physcians Button -->
-      <v-btn flat class="hidden-sm-and-down singlenav" @click="$router.push('/')">Physcians</v-btn>
+      <v-btn
+        flat
+        class="hidden-sm-and-down singlenav"
+        @click="$router.push('/')"
+        >Physcians</v-btn
+      >
       <v-spacer class="hidden-sm-and-down"></v-spacer>
-      <v-btn color="orange lighten-1" class="hidden-sm-and-down">Schedule</v-btn>
+      <v-btn color="orange lighten-1" class="hidden-sm-and-down"
+        >Schedule</v-btn
+      >
     </v-toolbar>
   </span>
 </template>
