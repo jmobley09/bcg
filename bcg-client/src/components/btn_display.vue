6201ad823ec1267cc359e9771597f7fc6c9a9e4a
jmobley <joshua.mobley.jm@gmail.com>
2019-06-09 20:38:55 -0400

    add carousel and started remainder of page

---
+++ bcg-client/src/components/btn_display.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-btn>One</v-btn>
+</template>
+
+<script>
+export default {
+  name: "btn_display",
+  data() {
+    return {
+    };
+  }
+};
+</script>
+
+<style scoped>
+</style>
