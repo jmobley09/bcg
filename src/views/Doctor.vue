4029c093ad553287a6d79cf9f2645ea63ef85f4b
jmobley <joshua.mobley.jm@gmail.com>
2019-07-08 20:01:45 -0400

    pulled the about information from old site

--- src/views/Doctor.vue
+++ src/views/Doctor.vue
@@ -1,6 +1,29 @@
 <template>
-  <div class="about">
-    <h1>This is the page for the doctor lamm</h1>
+  <div>
+    <p>
+      During his urology residency he developed an animal model for bladder cancer and found that Bacillus Calmette Guerin (the TB vaccine, BCG) inhibited tumor growth. After residency he and his family moved to San Antonio where Don began his academic career
+      as Assistant Professor of Surgery at the University of Texas Health Sciences Center. During his 9 years in Texas he rose in rank to Professor and Acting Chairman of the Division of Urology. His VA and NIH research resulted in clinical studies that led to
+      the FDA approval of BCG for bladder cancer.
+    </p>
+
+    <p>
+      Subsequent work by Dr. Lamm, then Chairman of the Department of Urology at West Virginia University, demonstrated that BCG immunotherapy is superior to Adriamycin and Mitomycin C chemotherapy for superficial bladder cancer, significantly reducing the
+      risk of bladder cancer progression to muscle invasion and metastasis. Dr. Lamm's work is in part responsible for bladder cancer being one of only 5 cancers in the United States that has seen a reduced mortality despite an increased incidence. This
+      reduction in mortality coincides with a reduction in the number of patients who lose their bladder as a result of radical cystoprostatectomy for muscle invasive or aggressive superficial disease.
+    </p>
+
+    <p>
+      Three years ago Don was recruited to the Mayo Clinic, Scottsdale as Vice Chair for Research in the Department of Urology. Having practiced and taught general urology for 3 decades, he has now decided to limit his practice exclusively to his primary
+      interest, urologic oncology. New to private practice, he greatly appreciates the good advice, help and welcome he has received from the private community. He is excited about the potential of expanding research in the private sector and hopes to establish
+      a cooperative research consortium in Arizona to advance the care of patients with genitourinary cancers.
+    </p>
+
+    <p>
+      Dr. Lamm has authored over 270 peer-reviewed medical and scientific articles, and in addition to new treatments of bladder cancer he has developed new approaches to the treatment of renal and prostate cancer as well. His primary interest, however,
+      continues to be BCG therapy of bladder cancer.
+    </p>
+
+    <p>Dr. Lamm and his wife, Wanda, have 4 grown children and 6 grandchildren.</p>
   </div>
 </template>
 
